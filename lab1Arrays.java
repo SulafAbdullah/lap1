b04db0407441a691cb177b6b4d47c9fd07cae243
SulafAbdullah <120080807+SulafAbdullah@users.noreply.github.com>
2023-04-07 01:59:08 +0000

    final

--- lab1Arrays.java
+++ lab1Arrays.java
@@ -5,24 +5,22 @@ public class lab1Arrays {
     public static void main( String[] args )
     {
         int[] nums = new int[5];
-        displayArray(readUserSequence(nums, 5), 5);
-        isPalindrome(displayArray(readUserSequence(nums, 5), 5), 5);
+       nums = readUserSequence(nums, 5);
+       displayArray(nums, 5);
+       isPalindrome(nums, 5);
+
+      
         
     }
 
     public static int[] readUserSequence(int[] arrayToFill , int limit){
         int[] nums = new int[5];
         Scanner scnr = new Scanner(System.in);
-        System.out.println("enter the first number: ");
-        nums[0]= scnr.nextInt();
-        System.out.println("enter the second number: ");
-        nums[1]= scnr.nextInt();
-        System.out.println("enter the third number: ");
-        nums[2]= scnr.nextInt();
-        System.out.println("enter the forth number: ");
-        nums[3]= scnr.nextInt();
-        System.out.println("enter the fifth number: ");
-        nums[4]= scnr.nextInt();
+        for(int i = 0 ; i < nums.length; i ++){
+          System.out.println("enter the "+(i+1)+" number: ");
+          nums[i]= scnr.nextInt();
+        }
+        
 
         scnr.close();
         return nums; 
